1fabf35bc52002cd4f246807d5c191a5a6405c46
Hasan <MHASAN@beckman.com>
2019-11-29 10:28:53 -0500

    vignette updated

--- vignettes/OPWeight.R
+++ vignettes/OPWeight.R
@@ -1,52 +1,62 @@
-## ----setup, echo = FALSE-------------------------------------------------
+## ----setup, echo = FALSE----------------------------------------------------
 knitr::opts_chunk$set(tidy = FALSE, cache = FALSE, autodep = TRUE)
 
-## ----loadlibs, message=FALSE, warning=FALSE------------------------------
+
+## ----loadlibs, message=FALSE, warning=FALSE---------------------------------
 # install OPWeight package
-source("https://bioconductor.org/biocLite.R")
-biocLite("OPWeight", suppressUpdates=TRUE)
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#     install.packages("BiocManager")
+# BiocManager::install("OPWeight")
 
 # load packages
-library("OPWeight")
-library("ggplot2")
-library("airway")
-library("DESeq2")
+library(OPWeight)
+library(ggplot2)
+library(airway)
+library(DESeq2)
 library(cowplot)
 library(tibble)
 library(qvalue)
 library(MASS)
 
-## ----dataProcessing, message=FALSE, warning=FALSE------------------------
+
+## ----dataProcessing, message=FALSE, warning=FALSE---------------------------
 data("airway", package = "airway")
 dds <- DESeqDataSet(se = airway, design = ~ cell + dex)
 dds <- DESeq(dds)
-de_res_air <- results(dds)
+de_res_air <- as.data.frame(results(dds))
 
-## ----colnames_de_res_air-------------------------------------------------
+
+## ----colnames_de_res_air----------------------------------------------------
 colnames(de_res_air)
 dim(de_res_air)
 
-## ----loadOPWeight, message=FALSE, warning=FALSE--------------------------
-covariates = de_res_air$baseMean + .0001  # add a small constant to make all values positive
+
+## ----loadOPWeight, message=FALSE, warning=FALSE-----------------------------
+# add a small constant to make all values positive
+covariates = de_res_air$baseMean + .0001  
 set.seed(123)
 # one should use more nrep to obtain more acurate results
 opw_results <- opw(pvalue = de_res_air$pvalue, covariate = covariates, nrep = 1000,
                    alpha = .1, tail = 2, effectType = "continuous", method = "BH")
 
-## ----outputsName---------------------------------------------------------
+
+## ----outputsName------------------------------------------------------------
 names(opw_results)
 
-## ----nullProp------------------------------------------------------------
+
+## ----nullProp---------------------------------------------------------------
 opw_results$nullProp
 
-## ----no.OfRejections-----------------------------------------------------
+
+## ----no.OfRejections--------------------------------------------------------
 opw_results$rejections
 
-## ----probVsWeight--------------------------------------------------------
+
+## ----probVsWeight-----------------------------------------------------------
 m = opw_results$totalTests
 testRanks = 1:m
-probs = opw_results$ranksProb
-weights = opw_results$weight
+probs = opw_results$dataOut$ranksProb
+weights = opw_results$dataOut$weight
 dat = tibble(testRanks, probs, weights)
 p_prob = ggplot(dat, aes(x = testRanks, y = probs)) + geom_point(size = .5, col="firebrick4") + 
     labs(x = "Test rank" , y = "p(rank | effect)")
@@ -54,7 +64,8 @@ p_weight = ggplot(dat, aes(x = testRanks, y = weights)) + geom_point(size = .5, 
     labs(x = "Test rank" , y = "Weight")
 plot_grid(p_prob, p_weight, labels = c("a", "b"))
 
-## ----summaryPlots--------------------------------------------------------
+
+## ----summaryPlots-----------------------------------------------------------
 Data <- tibble(pval=de_res_air$pvalue, covariate=de_res_air$baseMean)
 
 
@@ -82,7 +93,8 @@ title <- ggdraw() + draw_label("Airway: data summary")
 plot_grid(title, p, ncol = 1, rel_heights=c(0.1, 1)) 
 
 
-## ----dataAnalysis--------------------------------------------------------
+
+## ----dataAnalysis-----------------------------------------------------------
 # initial stage--------
 pvals = de_res_air$pvalue
 tests = qnorm(pvals/2, lower.tail = FALSE)
@@ -93,48 +105,55 @@ Data = tibble(pvals, covariates)
 OD <- Data[order(Data$covariates, decreasing=TRUE), ]
 Ordered.pvalue <- OD$pvals
 
-
 # estimate the true null and alternative test sizes------
 m = length(Data$pvals); m
 nullProp = qvalue(Data$pvals, pi0.method="bootstrap")$pi0; nullProp
 m0 = ceiling(nullProp*m); m0
 m1 = m - m0; m1
 
+# etimated test efects of the true altrnatives------------
+# keep top m1 tests for regression ---------
+tests[which(!is.finite(tests))] <- NA
+Data2 = add_column(Data, tests)
+OD2 <- Data2[order(Data2$tests, decreasing=TRUE), ][1:m1, ]
+
+test_effect_vec <-  OD2$tests
+
 # fit box-cox regression
 #--------------------------------
+bc <- MASS::boxcox(covariates ~ tests, data = OD2)
+lambda <- bc$x[which.max(bc$y)]
+model <- lm(covariates**lambda ~ tests, data = OD2)
 
-bc <- boxcox(covariates ~ tests)
-lambda <- bc$x[which.max(bc$y)]; lambda
-model <- lm(covariates^lambda ~ tests)
-
-# etimated test efects of the true altrnatives------------
-test_effect = if(m1 == 0) {0
-} else {sort(tests, decreasing = TRUE)[1:m1]}		# two-tailed test
 
 # for the continuous effects etimated mean effects
-mean_testEffect = mean(test_effect, na.rm = TRUE)
+mean_testEffect = mean(test_effect_vec, na.rm = TRUE)
 mean_testEffect
 mean_covariateEffect = model$coef[[1]] + model$coef[[2]]*mean_testEffect
 mean_covariateEffect
 
-## ----ranksProb-----------------------------------------------------------
+
+## ----ranksProb--------------------------------------------------------------
 set.seed(123)
 # compute ranks probability
 prob_cont <- sapply(1:m, prob_rank_givenEffect, et = mean_covariateEffect,
                    ey = mean_covariateEffect, nrep = 1000, m0 = m0, m1 = m1)
 
-## ----weights-------------------------------------------------------------
+
+## ----weights----------------------------------------------------------------
 # compute weihgts
 wgt = weight_continuous_nwt(alpha = .1, et = mean_testEffect, ranksProb = prob_cont)$w
 
-## ----results-------------------------------------------------------------
+
+## ----results----------------------------------------------------------------
 alpha = .1
 padj <- p.adjust(Ordered.pvalue/wgt, method = "BH")
 rejections_list = OD[which((padj <= alpha) == TRUE), ]
 n_rejections = dim(rejections_list)[1]
 n_rejections
 
-## ------------------------------------------------------------------------
+
+## ---------------------------------------------------------------------------
 opw_results2 <- opw(pvalue = pvals, covariate = covariates, weight = wgt, 
                      effectType = "continuous", alpha = .1, method = "BH")
 opw_results2$rejections
